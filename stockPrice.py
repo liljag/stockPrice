987d7056f12a703330a0fcf861b253f4ce7244b3
Lilja Yr Gudmundsdottir <liljayrgudmundsdottir@Liljas-MacBook-Pro.local>
2018-09-19 09:11:33 +0000

    dkjf

--- stockPrice.py
+++ stockPrice.py
@@ -1,5 +1,19 @@
 
 ans = 'y'
 while(ans == 'y'):
-	shares =input('Enter number of shares')
-	ans = input("Continue: ")
+	cont = True
+	while(cont):
+		try:
+			shares =int(input('Enter number of shares: '))
+			cont = False
+		except:
+			print('Invalid number!')
+	cont = True
+	while(cont):
+		try:
+			sent = input('Enter price (dollars, numerator, denominator): ')
+			price, num, den = int(sent.split())
+			cont = False
+		except:
+			print('Invalid price!')
+	ans = input('Continue: ')
